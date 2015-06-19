51687fef73f76968545c61060e04c58c055a4cf0
petlyakss <petlyakss@gmail.com>
2015-06-19 14:36:06 +0300

    Пытаюсь сделать список уже внесенных групп

--- module/timetable/views/lessons/index.php
+++ module/timetable/views/lessons/index.php
@@ -17,23 +17,6 @@ $this->params['breadcrumbs'][] = $this->title;
 $all_faculty = Faculty::find()->all();
 $all_groups = Groups::find()->all();
 $all_lessons = Lessons::find()->select('id_group')->distinct()->all();
-/*foreach ($all_faculty as $af){
-    foreach($all_lessons as $al){        
-        if($al['id_faculty'] == $af['faculty_id']){            
-            if($al['id_group'] != $tmp){
-                $uniq_group[$al['id_faculty']] = $al['id_group'];
-                $tmp = $al['id_group'];
-            } 
-    }
-}
-}
-foreach($all_lessons as $al){ 
-    $uniq_group[] = $al['id_group'];
-}*/
-
-//echo $result = array_unique($uniq_group);
-
-
 
 foreach($all_lessons as $all){
     
@@ -49,16 +32,24 @@ foreach($all_lessons as $all){
 
 foreach($res as $rr){
     $faculty_id = Speciality::findOne(["speciality_id" => $rr['id_speciality']]);
-    $arr[] = array(
+    $faculty_name = Faculty::findOne(["faculty_id" => $faculty_id['id_faculty']]);
+    $arr[$faculty_name["faculty_name"]][] = array(
         "id" => $rr['group_id'],
         "name" => $rr['main_group_name'],
-        "sp" => $faculty_id['id_faculty']
+        "speciality_id" => $faculty_id["speciality_id"],
+        "faculty_name" => $faculty_name["faculty_name"]
     );
 }
 
-var_dump($arr);
 
 
+
+sort($arr);
+reset($arr);
+while (list($key, $val) = each($arr)) {
+    $lists[$key][] = $val;
+}
+var_dump($lists);
 ?>
 <div class="lessons-index">
 
