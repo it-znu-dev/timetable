d2d4077be796e6fa73bb5fcbdc225d20070760e3
petlyakss <petlyakss@gmail.com>
2015-06-24 11:05:46 +0300

    0.8.9.3
    Перед изменением страницы выбора группы для ввода расписания

--- module/timetable/views/lessons/index.php
+++ module/timetable/views/lessons/index.php
@@ -13,71 +13,75 @@ use app\module\handbook\models\Speciality;
 
 $this->title = 'Розклад занять';
 $this->params['breadcrumbs'][] = $this->title;
-
-//$all_faculty = Faculty::find()->all();
-//$all_groups = Groups::find()->all();
-//$all_lessons = Lessons::find()->select('id_group')->distinct()->all();
-
-$all_lessons = Lessons::find()->where(['id_faculty' => 3])->select('id_group')->distinct()->all();
-
-foreach($all_lessons as $al){
-    //echo $al['id_group']."<br/>";
-    $group_tmp = Groups::findOne(["group_id" => $al['id_group']]);
-    if($group_tmp['parent_group'] == 0){
-        echo $group_tmp['main_group_name']."<br/>";
-    }else{
-        $group_name = Groups::findOne(["group_id" => $group_tmp['parent_group']]);
-        echo $group_name['main_group_name']."<br/>";
-    }
-} 
-//var_dump($all_lessons);
-/*
-foreach($all_lessons as $all){
-    
-    $group_tmp = Groups::findOne(["group_id" => $all['id_group']]);
-    
-    if($group_tmp['parent_group'] == 0){
-        $res[] = Groups::findOne(["group_id" => $all['id_group']]);
-    }else{
-        $res[] = Groups::findOne(["group_id" => $group_tmp['parent_group']]);
-    }
-     
-}*/
-/*
-foreach($res as $rr){
-    $faculty_id = Speciality::findOne(["speciality_id" => $rr['id_speciality']]);
-    $faculty_name = Faculty::findOne(["faculty_id" => $faculty_id['id_faculty']]);
-    $arr[$faculty_name["faculty_name"]][] = array(
-        "id" => $rr['group_id'],
-        "name" => $rr['main_group_name'],
-        "speciality_id" => $faculty_id["speciality_id"],
-        "faculty_name" => $faculty_name["faculty_name"]
-    );
-}
-
-
-var_dump($lists);*/
-
-echo "<hr/>";
-
+?>
+<div class="panel-group" role="tablist">
+<?php
 $all_faculty = Faculty::find()->orderBy('faculty_name ASC')->all();
+$count = 0;
 foreach($all_faculty as $af){
-    $count = 0;
+    unset($groups_array);
     $all_gr = Lessons::find()->where(['id_faculty' => $af['faculty_id']])->select('id_group')->distinct()->all();
     foreach($all_gr as $ag){
         $gr = Groups::find()->where(['group_id' => $ag['id_group']])->all();
         if($gr[0]['parent_group'] == 0){
-            $count++;
+            $groups_array[] = $gr[0]['group_id'];
         }else{
-            continue;
-            $count++;
+            $groups_array[] = $gr[0]['parent_group'];
         }
     }
-    //echo $count;
-    //$a = Groups::find()->where(['id_speciality' => $af['id_speciality'], 'parent_group' => 0])->count();
-    echo $af['faculty_name']." = ".$count."<br/>";
+    $groups_list = array_unique($groups_array);
+    ?>
+    <div class="panel panel-default">
+        <div class="panel-heading" role="tab" id="collapseListGroupHeading<?php echo $count; ?>">
+        <h4 class="panel-title">
+          <a class="collapsed" role="button" data-toggle="collapse" href="#collapseListGroup<?php echo $count; ?>" aria-expanded="false" aria-controls="collapseListGroup<?php echo $count; ?>">
+            <?php echo "<h4 class='small_margins'>".$af['faculty_name']."\t<span class='label label-primary'>".count($groups_list)."</span></h4>";?>
+          </a>
+        </h4>
+      </div>
+      <div style="height: 0px;" aria-expanded="false" id="collapseListGroup<?php echo $count; ?>" class="panel-collapse collapse" role="tabpanel" aria-labelledby="collapseListGroupHeading<?php echo $count; ?>">
+        <ul class="list-group">    
+    <?php    
+    foreach($groups_list as $grl){
+        
+        $sem = date("n");
+        if($sem > 8){
+            $semester_for_editor = 1;
+        }else{
+            $semester_for_editor = 2;
+        }
+        $gr = Groups::find()->where(['group_id' => $grl])->all();
+        $course_get = date("Y") - $gr[0]['inflow_year'];
+        $speciality_id = $gr[0]['id_speciality'];
+        $group_name = $gr[0]['main_group_name'];
+        $faculty_id = $af['faculty_id'];
+        
+        echo  '<li class="list-group-item">
+                <a href="index.php?r=timetable/lessons/editor&semester_for_editor=1&course_get='.$course_get.
+                    '&faculty_id='.$faculty_id.
+                    '&speciality_id='.$speciality_id.
+                    '&group_id='.$grl.'" target="_blank">'.
+                    $group_name.' 1-й семестр
+                </a>
+                '."\t ||\t ".'    
+                <a href="index.php?r=timetable/lessons/editor&semester_for_editor=2&course_get='.$course_get.
+                    '&faculty_id='.$faculty_id.
+                    '&speciality_id='.$speciality_id.
+                    '&group_id='.$grl.'" target="_blank">'.
+                    $group_name.' 2-й семестр
+                </a>
+               </li>';
+    }
+    $count++;
+    ?>
+    </ul>        
+      </div>
+    </div>
+<?php    
 }
 ?>
+</div> 
+
 <div class="lessons-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
